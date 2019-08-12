429a4af97f5862e81705825460ae7d9f5bde540c
u2 <zhangyaning1985@gmail.com>
2019-08-12 18:20:48 +0800

    chore: remove unstable bench

--- benches/stable.rs
+++ benches/stable.rs
@@ -45,56 +45,7 @@ fn bench(c: &mut Criterion) {
         })
     });
 
-    let unstable_add = Fun::new("UnstableAdd", |b, _| {
-        let mut s = StableBloomFilter::new_unstable(200, 0.1);
-        let mut data = Vec::new();
-        for i in 0..1000 {
-            data.push(i.to_string().into_bytes());
-        }
-
-        b.iter(|| {
-            for i in data.iter() {
-                s.add(i);
-            }
-        })
-    });
-
-    let unstable_test = Fun::new("UnstableTest", |b, _| {
-        let s = StableBloomFilter::new_unstable(200, 0.1);
-        let mut data = Vec::new();
-        for i in 0..1000 {
-            data.push(i.to_string().into_bytes());
-        }
-
-        b.iter(|| {
-            for i in data.iter() {
-                s.test(i);
-            }
-        })
-    });
-
-    let unstable_test_and_add = Fun::new("UnstableTestAndAdd", |b, _| {
-        let mut s = StableBloomFilter::new_unstable(200, 0.1);
-        let mut data = Vec::new();
-        for i in 0..1000 {
-            data.push(i.to_string().into_bytes());
-        }
-
-        b.iter(|| {
-            for i in data.iter() {
-                s.test_and_add(i);
-            }
-        })
-    });
-
-    let functions = vec![
-        add,
-        test,
-        test_and_add,
-        unstable_add,
-        unstable_test,
-        unstable_test_and_add,
-    ];
+    let functions = vec![add, test, test_and_add];
     c.bench_functions("StableBloomFilter", functions, 0);
 }
 
